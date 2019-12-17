f22fb13d19888c9b09602b5f37ab1c88e8916849
Fabian Vallejos <vallejosfab@gmail.com>
2019-12-17 13:43:33 -0300

    Added MySQL <8 install script for ubuntu 18+

---
+++ install-mysql.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "Updating repositories"
+
+apt update
+
+echo "Installing packages"
+
+apt install mysql-server -y
+
+echo "Configuring MySQL"
+
+mysql_secure_installation <<EOF
+
+n
+root123
+root123
+y
+y
+y
+y
+EOF
+
+echo "Done!"
+
